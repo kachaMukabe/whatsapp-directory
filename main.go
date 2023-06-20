028588a7e52e580cd3deb18fce8f08c0d43559c1
kachaMukabe <kachamukabe@gmail.com>
2023-06-20 20:17:38 +0200

    Testing new version for render

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -13,6 +14,7 @@ func verifyWebhook(c *gin.Context) {
 	challenge := c.Query("hub.challenge")
 	verifyToken := c.Query("hub.verify_token")
 	token := os.Getenv("VERIFYTOKEN")
+	log.Print(token)
 
 	if mode == "subscribe" && verifyToken == token {
 		c.String(http.StatusOK, "%s", challenge)
@@ -26,15 +28,20 @@ func handleMessageNotification(c *gin.Context) {
 	}
 	fmt.Println(notification)
 	fmt.Printf("%#v\n", notification)
+	fmt.Println(notification.Entry[0].Changes[0].Value.Messages)
 
 	c.JSON(http.StatusOK, notification.Entry[0].Changes[0].Value.Messages[0].Text.Body)
 
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	router := gin.Default()
 	router.GET("/webhook", verifyWebhook)
 	router.POST("/webhook", handleMessageNotification)
 
-	router.Run("localhost:8000")
+	router.Run(":" + port)
 }
